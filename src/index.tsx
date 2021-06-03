bc8dff214ddede04b269f52c6270c1ca1f435eb8
Philipp Hoenisch <philipp@hoenisch.at>
2021-06-04 08:07:40 +1000

    SSE example

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import {SSEProvider, useSSE} from "react-hooks-sse";
+
+
+const useEventSource = (url) => {
+    const [data, updateData] = useState(null);
+
+    useEffect(() => {
+        const source = new EventSource(url);
+
+        source.onmessage = function logEvents(event) {
+            console.log(event);
+            updateData(JSON.parse(event.data));
+        }
+    }, [])
+
+    return data;
+}
+
+const Rate = () => {
+    const data = useEventSource("http://wfvgbnvdow2ja4wdodbvhh4ikdr6a7rjltbjsriebzlkxw4u4zwnflqd.onion:3030/api/rate/btc-xmr");
+    // const data = useEventSource("http://159.196.146.253:3030/api/rate/btc-xmr");
+    // const data = useEventSource("http://localhost:3030/api/rate/btc-xmr");
+    return (
+        <>
+            {JSON.stringify(data)}
+        </>
+    );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {/*<SSEProvider endpoint="http://localhost:3030/api/rate/btc-xmr">*/}
+          <h1>Subscribe & update to SSE event</h1>
+          <Rate />
+      {/*</SSEProvider>*/}
   </React.StrictMode>,
   document.getElementById("root")
 );
