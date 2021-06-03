90064655b12cd507e55de84bffb236919ca722e2
Philipp Hoenisch <philipp@hoenisch.at>
2021-06-04 09:20:36 +1000

    WS example

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,39 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import {SSEProvider, useSSE} from "react-hooks-sse";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
 
-const useEventSource = (url) => {
-    const [data, updateData] = useState(null);
+export const WebSocketDemo = () => {
+    //Public API that will echo messages sent to it back to the client
+    const socketUrl = 'ws://wfvgbnvdow2ja4wdodbvhh4ikdr6a7rjltbjsriebzlkxw4u4zwnflqd.onion:3030/api/rate/btc-xmr';
+    // const socketUrl = 'ws://localhost:3030/api/rate/btc-xmr';
 
-    useEffect(() => {
-        const source = new EventSource(url);
+    const {
+        lastJsonMessage,
+        readyState,
+    } = useWebSocket(socketUrl);
 
-        source.onmessage = function logEvents(event) {
-            console.log(event);
-            updateData(JSON.parse(event.data));
-        }
-    }, [])
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: 'Connecting',
+        [ReadyState.OPEN]: 'Open',
+        [ReadyState.CLOSING]: 'Closing',
+        [ReadyState.CLOSED]: 'Closed',
+        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[readyState];
 
-    return data;
-}
-
-const Rate = () => {
-    const data = useEventSource("http://wfvgbnvdow2ja4wdodbvhh4ikdr6a7rjltbjsriebzlkxw4u4zwnflqd.onion:3030/api/rate/btc-xmr");
-    // const data = useEventSource("http://159.196.146.253:3030/api/rate/btc-xmr");
-    // const data = useEventSource("http://localhost:3030/api/rate/btc-xmr");
     return (
-        <>
-            {JSON.stringify(data)}
-        </>
+        <div>
+            <span>The WebSocket is currently {connectionStatus}</span>
+            <br/>
+            {lastJsonMessage ? <span>Last message: {JSON.stringify(lastJsonMessage)}</span> : null}
+        </div>
     );
 };
 
 ReactDOM.render(
-  <React.StrictMode>
-      {/*<SSEProvider endpoint="http://localhost:3030/api/rate/btc-xmr">*/}
-          <h1>Subscribe & update to SSE event</h1>
-          <Rate />
-      {/*</SSEProvider>*/}
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <h1>Subscribe to websockets</h1>
+        <WebSocketDemo/>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
